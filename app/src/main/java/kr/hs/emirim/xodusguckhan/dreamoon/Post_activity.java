745fc58fd907ed7674608342b704b63fb59fc52e
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-08-01 15:49:32 +0900

    [ADD]guck0801_01

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Post_activity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Post_activity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Post_activity extends AppCompatActivity {
+    String[] items = {"현몽", "역몽", "길몽", "흉몽", "태몽", "계시몽", "자각몽(루시드드림)", "영몽", "실몽"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+
     }
 }
