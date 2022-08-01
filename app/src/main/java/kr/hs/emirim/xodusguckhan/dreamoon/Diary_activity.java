f751b15591956789a932ab83a8c4b14e3bd48a57
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-08-01 14:46:18 +0900

    [ADD] xodus Activity 추가

---
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Diary_activity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.xodusguckhan.dreamoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Diary_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
