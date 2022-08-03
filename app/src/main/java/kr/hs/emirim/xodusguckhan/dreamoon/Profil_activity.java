0e5959d83c35f3c2120c767ea9986a93052ed561
kwonha <s2118@e-mirim.hs.kr>
2022-08-03 15:13:06 +0900

    [ADD] han 해몽 액티비티 화면 일부 완성

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Profil_activity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Profil_activity.java
@@ -10,11 +10,18 @@ import android.widget.EditText;
 public class Profil_activity extends AppCompatActivity {
     Button btnUpd = findViewById(R.id.btn_upd);
     EditText editIntro = findViewById(R.id.edit_intro);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
-
+        btnUpd.setOnClickListener(btnListener);
     }
 
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 }
