89489cade170631b9c189831086686ecdf237a9c
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-09-06 19:59:33 +0900

    [UPDATE]guck0906

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(getApplicationContext(), Diary_activity.class);
-
+            startActivityForResult(intent, 0);
         }
     };
 
