642e0e8dc6366ede66cfed9220cbe99da8973be7
kwonha <s2118@e-mirim.hs.kr>
2022-09-21 01:17:12 +0900

    [ADD] han Profil_activity.java 일부 완성

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Profil_activity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Profil_activity.java
@@ -6,22 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Profil_activity extends AppCompatActivity {
+    EditText textName = findViewById(R.id.edit_name);
+    EditText textIntro = findViewById(R.id.edit_intro);
+    String strTextName = textName.getText().toString();
+    String strTextIntro = textIntro.getText().toString();
     Button btnUpd = findViewById(R.id.btn_upd);
-    EditText editIntro = findViewById(R.id.edit_intro);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
-        btnUpd.setOnClickListener(btnListener);
+
+        btnUpd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textName.setText(strTextName);
+                textIntro.setText(strTextIntro);
+            }
+        });
     }
 
-    View.OnClickListener btnListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-        }
-    };
 }
