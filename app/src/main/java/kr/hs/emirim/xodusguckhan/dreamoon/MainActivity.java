d56484ee70f392299b02eefbd6c799f3cfa45770
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-09-22 20:01:05 +0900

    [ADD] xodus 캘린더 버전 변경

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
@@ -1,13 +1,17 @@
 package kr.hs.emirim.xodusguckhan.dreamoon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+//import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    //private MaterialCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
