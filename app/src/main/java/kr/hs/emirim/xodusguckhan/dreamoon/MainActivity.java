bcaf1321a0b14bf7a3c294a999664a54f56f095a
xodus05 <s2121@e-mirim.hs.kr>
2022-09-27 19:52:20 +0900

    [ADD] xodus 캘린더 날짜 설정 완료

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
@@ -2,33 +2,33 @@ package kr.hs.emirim.xodusguckhan.dreamoon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-//import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //private MaterialCalendarView calendarView;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button Mbtn = findViewById(R.id.main_tab);
-        Mbtn.setOnClickListener(mbtnListener);
-        Button calendar_tab = findViewById(R.id.calendar_tab);
+        calendarView = (CalendarView) findViewById(R.id.calendar);
 
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                Log.i("디버깅", "선택한 날짜는" + i + "년" + (i1 + 1) + "월" + i2 + "일");
+            }
+        });
     }
-    View.OnClickListener mbtnListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(getApplicationContext(), Diary_activity.class);
-            startActivityForResult(intent, 0);
-        }
-    };
-
 
 }
