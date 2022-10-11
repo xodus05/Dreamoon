bb9d59d53b69d82f13fa6511e32e0790052551d6
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-10-11 19:45:09 +0900

    update_1011

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
@@ -1,34 +1,51 @@
 package kr.hs.emirim.xodusguckhan.dreamoon;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Calendar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private CalendarView calendarView;
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calendarView = (CalendarView) findViewById(R.id.calendar);
+        bottomNavigationView = findViewById(R.id.bottomNavi);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        //처음화면
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new Fragment1()).commit(); //FrameLayout에 fragment.xml 띄우기
+
+        //바텀 네비게이션뷰 안의 아이템 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                Log.i("디버깅", "선택한 날짜는" + i + "년" + (i1 + 1) + "월" + i2 + "일");
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+                    case R.id.i1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment1()).commit();
+                        break;
+                    case R.id.i2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment2()).commit();
+                        break;
+                    case R.id.i3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment3()).commit();
+                        break;
+                    case R.id.i4:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment4()).commit();
+                        break;
+                    case R.id.i5:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment5()).commit();
+                        break;
+                }
+                return true;
             }
         });
     }
-
 }
