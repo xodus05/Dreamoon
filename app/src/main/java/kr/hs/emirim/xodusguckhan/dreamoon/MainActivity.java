057d0be51ed434b0aebcc2206316e80e891933a4
xodus05 <s2121@e-mirim.hs.kr>
2022-10-13 19:24:13 +0900

    [ADD] xodus 네비게이션 바 캘린더 이동 설정 완료

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/MainActivity.java
@@ -1,26 +1,28 @@
 package kr.hs.emirim.xodusguckhan.dreamoon;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    private CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavi);
+        calendarView = (CalendarView) findViewById(R.id.calendar);
 
         //처음화면
-        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new Fragment1()).commit(); //FrameLayout에 fragment.xml 띄우기
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new Fragment()).commit(); //FrameLayout에 fragment.xml 띄우기
 
         //바텀 네비게이션뷰 안의 아이템 설정
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.i2:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment2()).commit();
                         break;
-                    case R.id.i3://
+                    case R.id.i3:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Fragment3()).commit();
                         break;
                     case R.id.i4:
@@ -45,6 +47,13 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 return true;
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                Log.i("디버깅", "선택한 날짜는" + i + "년" + (i1 + 1) + "월" + i2 + "일");
             }
         });
     }
