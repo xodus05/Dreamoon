69d586f6f1a2c81c4c463bd8d7d024ed73cc2f76
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-10-25 18:45:59 +0900

    [ADD] guck 1025 fragment_2 community로 이름 변경

--- app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Fragment2.java
+++ app/src/main/java/kr/hs/emirim/xodusguckhan/dreamoon/Fragment2.java
@@ -13,6 +13,6 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        return inflater.inflate(R.layout.community, container, false);
     }
 }
